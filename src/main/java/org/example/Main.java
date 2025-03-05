f1c96d8cc36b48c7c64197a90ccaf54d34fa25a4
timothy lefkowitz <timothy.lefkowitz@gmail.com>
2025-03-04 18:09:06 -0600

    Feat: reverseString

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,18 @@ package org.example;
 
 public class Main {
 
+    public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
     public void reverseString(char[] s){
         // Initialize two pointers
         int left = 0;
@@ -25,6 +37,10 @@ public class Main {
         System.out.println(new String(s));
     }
 
+    public static void printIntArray(int[] arr) {
+        System.out.printf(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!, This is my prac file");
 
@@ -37,6 +53,17 @@ public class Main {
         solution.reverseString(test1);
         System.out.println("Test 1 After: ");
         printArray(test1);
+
+        //Two Sums test
+        System.out.println("\n Testing twoSum: ");
+        int[] nums1 = {2,7,11,15};
+        int target1 = 9;
+        System.out.println(" Test 1 Input: nums = ");
+        printIntArray(nums1);
+        System.out.println("Target: " + target1);
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Test 1 Output: ");
+        printIntArray(result1);
 
     }
 }
